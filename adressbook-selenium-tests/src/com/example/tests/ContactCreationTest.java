07a7b62635e684cd1cede9681b1af0deb378ca2a
sdemidov <Sergey.Demidov@billing.ru>
2015-08-31 11:52:30 +0300

    D32_fix_01
    fix comments

---
+++ adressbook-selenium-tests/src/com/example/tests/ContactCreationTest.java
@@ -0,0 +1,30 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class ContactCreationTest extends TestBase {
+
+	@Test
+	public void contactNonEmptyCreationTest() throws Exception {
+		openMainPage();
+		addName();
+		GroupName contact = new GroupName();
+		contact.firstname = "firstname";
+		contact.lastname = "lastname";
+		contact.address = "address";
+		contact.home = "home";
+		contact.mobiletel = "mobiletel";
+		contact.worktel = "worktel";
+		contact.email = "email";
+		contact.email2 = "email2";
+		contact.bday = "1";
+		contact.bmonth = "May";
+		contact.byear = "2000";
+		contact.address2 = "address2";
+		contact.phone2 = "phone2";
+		fillContactForm(contact);
+		submitContactCreation();
+		returnToContactPage();
+	}
+
+}
---
+++ adressbook-selenium-tests/src/com/example/tests/GroupCreationTest.java
@@ -0,0 +1,30 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupCreationTest extends TestBase {
+	@Test
+	public void groupNonEmptyCreationTest() throws Exception {
+		openMainPage();
+		openGroupPage();
+		inityGroupNewCreation();
+		GroupName group = new GroupName();
+		group.name = "group 1";
+		group.header = "header 1";
+		group.footer = "footer 1";
+		fillGroupForm(group);
+		submitGroupCreation();
+		returnToGroupPage();
+	}
+
+	@Test
+	public void groupEmptyCreationTest() throws Exception {
+		openMainPage();
+		openGroupPage();
+		inityGroupNewCreation();
+		GroupName group = new GroupName();
+		fillGroupForm(new GroupName("", "", ""));
+		submitGroupCreation();
+		returnToGroupPage();
+	}
+}
